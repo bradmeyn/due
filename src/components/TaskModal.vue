74321ea455477c63ab46f585f88939c7df7fbd46
Bradley Meyn <BradleyMey@westrac.com.au>
2023-12-14 16:18:06 +1100

    test push

--- src/components/TaskModal.vue
+++ src/components/TaskModal.vue
@@ -1,52 +1,31 @@
-
-Your Vue.js component code mostly looks correct, but there are a couple of things to note:
-
-Props Declaration: You are using defineProps to declare the isModalActive prop. This is generally correct, but the syntax can be a bit different depending on how you want to define your props. If isModalActive is a Boolean, you might want to define it more explicitly:
-
-javascript
-Copy code
-const props = defineProps({
-  isModalActive: Boolean
-})
-This way, you can provide type checking and default values if needed.
-
-Usage of Props: In your template, you correctly use v-show="isModalActive". This will show or hide the modal based on the value of isModalActive. Just make sure that isModalActive is correctly passed as a prop from the parent component.
-
-Styling and Layout: Your CSS seems fine for a basic modal layout. The modal will be centered on the screen with a semi-transparent black background. The modal itself has a minimum width and a border. You might want to add more styles for the modal contents, buttons, and inputs, depending on your design requirements.
-
-Form Handling: Your form currently has an input and a save button, but there's no action or method defined for the form. Depending on your requirements, you might want to add a v-on:submit handler to process the form data.
-
-Closing the Modal: You have a close button, but there's no event handler attached to it. You might want to add a click event handler to change the isModalActive prop or emit an event to the parent component to close the modal.
-
-Accessibility Considerations: Ensure that your modal is accessible. This includes proper keyboard navigation, focus management, and aria attributes for screen readers.
-
-Here's a revised version with some of these considerations:
-
-vue
-Copy code
 <script setup>
-import {  emit } from 'vue';
-
 const props = defineProps({
-  isModalActive: Boolean
+  isModalActive: Boolean,
 });
-
-const closeModal = () => {
-  emit('update:isModalActive', false);
-};
-
 </script>
 
-<template >
-  <div class="modal__container" @click.self="closeModal">
+<template>
+  <div class="modal__container">
     <div class="modal">
-      <div>
-        <h2>New Task</h2> 
-        <button class="modal__close-btn" @click="closeModal">Close</button>
-      </div>
+      <header>
+        <h2>New Task</h2>
+        <button class="modal__close-btn" @click="closeModal">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="16"
+            width="12"
+            viewBox="0 0 384 512"
+          >
+            <path
+              fill="#fff"
+              d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"
+            />
+          </svg>
+        </button>
+      </header>
       <form @submit.prevent="handleSubmit">
         <label for="detail">Description</label>
-        <input type="text" class="task-detail" name="detail">
+        <input type="text" class="task-detail" name="detail" />
         <button type="submit" class="modal__save-btn">Save</button>
       </form>
     </div>
@@ -54,36 +33,85 @@ const closeModal = () => {
 </template>
 
 <style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 1rem;
+}
+
+h2 {
+  font-size: 2rem;
+  color: #fff;
+  font-weight: semibold;
+}
+
 .modal__container {
-    position: fixed;
-    top: 0;
-    left:0;
-    width: 100%;
-    height: 100vh;
-    background-color: rgba(0, 0, 0, 0.8);
-    border: solid 1px blue;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.6);
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 
 .modal {
-    min-width: 20rem;
-    border: solid 1px salmon;
+  min-width: 20rem;
+  padding: 1rem;
+  background-color: var(--dark);
+  border-radius: 5px;
+}
+
+.modal__close-btn {
+  color: #fff;
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  outline: none;
+  padding: 0;
+}
+
+label {
+  font-size: 0.8rem;
+  display: block;
+  margin-bottom: 0.2rem;
+  color: slategray;
+  font-weight: semibold;
+}
+
+input {
+  display: block;
+  width: 100%;
+  padding: 0.5rem;
+  border-radius: 5px;
+  border: 0.5px solid slategray;
+  margin-bottom: 1rem;
+  background-color: var(--dark);
+}
+
+input:focus {
+  outline: none;
+  border: 0.5px solid var(--green);
 }
 
 .modal__save-btn {
-    display: block;
-    border-radius: 5px;
-    text-align: center;
-    padding: 0.5rem;
-    background-color: var(--green);
-    color: white;
-    width: 100%;
+  display: block;
+  border-radius: 5px;
+  text-align: center;
+  padding: 0.5rem;
+  font-size: 1.2rem;
+  background-color: var(--green);
+  color: white;
+  width: 100%;
+  font-weight: bold;
+  border: none;
+  cursor: pointer;
 }
 
-
 h2 {
-    color: var(--light);
+  color: var(--light);
 }
 </style>
